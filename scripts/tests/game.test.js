ad93a0dabac407865735ccf76e77f3a709f1bc87
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-12 06:49:10 +0000

    feat: Add a beforeAll function to game.test.js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,11 @@
 /**
  * @jest-environment jsdom
  */
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});

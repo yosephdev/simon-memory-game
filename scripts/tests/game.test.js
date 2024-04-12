ed78680ebdb1d731947a8fabb4998c99faa94397
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-12 07:02:09 +0000

    feat: Create an object that will hold the game state

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -9,3 +9,12 @@ beforeAll(() => {
     document.write(fileContents);
     document.close();
 });
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+});

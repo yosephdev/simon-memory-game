c711fccfce48ec9506cdeaaec2992e703891aa15
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 13:26:43 +0000

    feat: Implement showTurns function to display game turns

--- scripts/game.js
+++ scripts/game.js
@@ -16,6 +16,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
 }
 
 function showScore() {
@@ -31,5 +32,15 @@ function lightsOn(circ) {
     }, 400);
 }
 
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };

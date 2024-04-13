526fb1942d19ce884955a0a37a58459847d1fbc3
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 13:35:20 +0000

    test: Ensure game object contains turnNumber key

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -28,6 +28,9 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {

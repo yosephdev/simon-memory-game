8d2216d9e487d95b08c3bc106f1e3772ddf092ab
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 14:49:03 +0000

    feat: Add playerTurn function to handle player moves and game logic

--- scripts/game.js
+++ scripts/game.js
@@ -57,4 +57,18 @@ function showTurns() {
     }, 800);
 }
 
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
 module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };

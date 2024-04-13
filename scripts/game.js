5d22415a69fa1ffda8487277eadced143f3a549d
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 13:47:46 +0000

    feat: Add event listener to circle elements for player interaction

--- scripts/game.js
+++ scripts/game.js
@@ -10,6 +10,19 @@ function newGame() {
     game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
+
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
+
     showScore();
     addTurn();
 }

487ed7a9fb794311a25634bc41bd8b42f29ab831
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 15:33:56 +0000

    test: Ensure clicking during computer sequence does not register player move

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -105,4 +105,10 @@ describe("gameplay works correctly", () => {
         showTurns();
         expect(game.turnInProgress).toBe(true);
     });
+    test("clicking during computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
+    });
 });

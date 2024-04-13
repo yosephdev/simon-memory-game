1451b151501def2539b80d8332e9eb7e00a07f12
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 13:28:22 +0000

    feat: Import showTurns function into game.test.js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");

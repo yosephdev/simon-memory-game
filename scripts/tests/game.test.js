65cb87ec77d7e951073bf0881ce59225a90392cb
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 13:58:38 +0000

    fix: Fix test expectation order in newGame block

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -35,23 +35,23 @@ describe("game object contains correct keys", () => {
 
 describe("newGame works correctly", () => {
     beforeAll(() => {
-        game.score = 42;  
+        game.score = 42;
         game.playerMoves = ["button1", "button2"];
-        game.currentGame = ["button1", "button2"]; 
-        document.getElementById("score").innerText = "42";     
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
         newGame();
-    });  
+    });
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
-    });       
+    });
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should display 0 for the element with id of score", () => {
-        expect(document.getElementById("score").innerText).toEqual(0);
-    });
     test("should add one move to the computer's game array", () => {
         expect(game.currentGame.length).toBe(1);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual("0");
     });
     test("expect data-listener to be true", () => {
         newGame();

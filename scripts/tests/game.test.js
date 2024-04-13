eb38ef50d138606255caa1820544c9dda95ff1bf
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-13 14:55:39 +0000

    test: Import playerTurn function into game.test.js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
